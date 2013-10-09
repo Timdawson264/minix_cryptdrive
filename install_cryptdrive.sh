918d4bcab38696acb0af9c245f676544609b1461
tim <tim@bob.(none)>
2013-10-10 01:25:56 +1300

    fixed permissions

--- install_cryptdrive.sh
+++ install_cryptdrive.sh
@@ -1,7 +1,9 @@
-urlget http://192.168.1.15/~tim/getcryptdrive.php | gzip -cryptdrive.tar;
+urlget http://192.168.1.15/~tim/getcryptdrive.php | gzip -d > cryptdrive.tar;
 
 #cat cryptdrive.tar | tar -tf - | grep '\./.' | while read x; do rm -rf $x; done;
 
 cat cryptdrive.tar | tar -xf - ;
+
+cat cryptdrive.tar | tar -tf - | grep '\./.' | while read x; do chown bin:0 $x; done;
 
 rm cryptdrive.tar.gz;
