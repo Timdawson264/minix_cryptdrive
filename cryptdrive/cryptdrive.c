f0186cf7334070f782d5df5775409706bea17b55
tim <tim@bob.(none)>
2013-10-12 15:18:20 +1300

    made it look nice

--- cryptdrive/cryptdrive.c
+++ cryptdrive/cryptdrive.c
@@ -18,53 +18,52 @@ int device_caller; /*pid of caller*/
  *===========================================================================*/
 PUBLIC void driver_task(void)
 {
-/* Main program of any device driver task. */
+	/* Main program of any device driver task. */
 
-  int r, proc_nr;
-  message mess;
+	int r, proc_nr;
+	message mess;
 
-  /* Here is the main loop of the disk task.  It waits for a message, carries
-   * it out, and sends a reply.
-   */
-  while (TRUE) {
-
-	/* Wait for a request to read or write a disk block. */
-	if(receive(ANY, &mess) != OK) continue;
-
-	device_caller = mess.m_source;
-	proc_nr = mess.PROC_NR;
-
-	/* Now carry out the work. */
-	switch(mess.m_type) {
-
-	/*
-	case DEV_READ:	
-	case DEV_WRITE:	 
-	case DEV_GATHER: 
-	case DEV_SCATTER: 
+	/* Here is the main loop of the disk task.  It waits for a message, carries
+	* it out, and sends a reply.
 	*/
-	
-	case TASK_REPLY:/* relay reply back to caller */
-		mess.REP_PROC_NR = proc_nr;
-		send(device_caller, &mess);
-		
-	default: 
-		/*proxy message to at_wini*/
-		mess.m_source = mess.PROC_NR; /*make this the source*/
-		mess.PROC_NR = DRVR_PROC_NR	/* make at_wini the destination */
-		
+	while (TRUE) {
+
+		/* Wait for a request to read or write a disk block. */
+		if(receive(ANY, &mess) != OK) continue;
+
+		device_caller = mess.m_source;
+		proc_nr = mess.PROC_NR;
+
+		/* Now carry out the work. */
+		switch(mess.m_type) {
+
+			/*
+			case DEV_READ:	
+			case DEV_WRITE:	 
+			case DEV_GATHER: 
+			case DEV_SCATTER: 
+			*/
+
+			case TASK_REPLY:/* relay reply back to caller */
+				mess.REP_PROC_NR = proc_nr;
+				send(device_caller, &mess);
+
+			default: 
+				/*proxy message to at_wini*/
+				mess.m_source = mess.PROC_NR; /*make this the source*/
+				mess.PROC_NR = DRVR_PROC_NR	/* make at_wini the destination */
+		}
+
+		r = EDONTREPLY;
+		/* Finally, prepare and send the reply message. */
+		if (r != EDONTREPLY) {
+			mess.m_type = TASK_REPLY;
+			mess.REP_PROC_NR = proc_nr;
+			/* Status is # of bytes transferred or error code. */
+			mess.REP_STATUS = r;	
+			send(device_caller, &mess);
+		}
 	}
-	
-	r = EDONTREPLY;
-	/* Finally, prepare and send the reply message. */
-	if (r != EDONTREPLY) {
-		mess.m_type = TASK_REPLY;
-		mess.REP_PROC_NR = proc_nr;
-		/* Status is # of bytes transferred or error code. */
-		mess.REP_STATUS = r;	
-		send(device_caller, &mess);
-	}
-  }
 }
 
 
